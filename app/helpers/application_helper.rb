244f1b7a23ef466815aaa3ee7c8c2adbee7a8f79
Vincent Lahaye <vincent@carredastuce.com>
2017-10-30 16:53:58 +0100

    Add title

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -4,4 +4,14 @@ module ApplicationHelper
     default_url = "http://kitt.lewagon.com/placeholder/users/ssaunier"
     url = "http://gravatar.com/avatar/#{gravatar_id}.png?s=#{size}&d=#{CGI::escape(default_url)}"
   end
+
+  def page_title(separator = " - ")
+    [content_for(:title), 'Rails Directory'].compact.join(separator)
+  end
+
+  def page_heading(title)
+    content_for(:title){ title }
+    content_tag(:h1, title)
+  end
+
 end
